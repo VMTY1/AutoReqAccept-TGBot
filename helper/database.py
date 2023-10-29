e14c404e074a6e08cc3fa0f2706e113124d7a25e
Snowball <108003920+Snowball-0@users.noreply.github.com>
2023-10-29 17:01:57 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -20,7 +20,7 @@ class Database:
         )
 
     async def add_user(self, b, m):
-        u = m
+        u = m.from_user
         if not await self.is_user_exist(u.id):
             user = self.new_user(u.id)    
             await self.col.insert_one(user)
