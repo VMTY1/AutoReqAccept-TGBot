fb0f861885f25910e6e2d54a8686e6469266c203
Snowball <108003920+Snowball-0@users.noreply.github.com>
2023-10-31 06:48:24 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -40,6 +40,7 @@ async def Settings(bot: Client, msg: Message):
         bool_leave = get_bool_leave_msg(msg.from_user.id)
         await msg.reply_text(bool_approve)
         print(type(bool_approve))
+        print(bool_approve.get('bool_approve')
         await msg.reply_text(bool_leave)
 
         if bool_approve and bool_leave:
